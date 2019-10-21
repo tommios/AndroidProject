2a78708184a27a8d009432597fac22bcf33dc7eb
[tommios] <[tommios.net@gmail.com]>
2019-10-21 15:01:25 +0300

    Create App ToDoList

---
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+
+        items = new ArrayList<String>();
+
+        readItems();
+
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        //items.add("Long press to delete any entry");
+        //items.add("20.10.2019 - Курсовой проект по Android");
+
+
+        setupListViewListener();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();  // получаем абсолютный путь к каталогу в файловой системе
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Удалить элемент в массиве по позиции
+                        items.remove(pos);
+                        // Обновляем адаптер
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        // Возвращаем true, длинное событие клика (помечаем его как обработанное)
+                        return true;
+                    }
+
+                });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+}
